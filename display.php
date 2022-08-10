6046849aca9284299566117474b7fc514742a203
Gauthier Baras <101516806+Gauthier5021@users.noreply.github.com>
2022-08-10 16:28:27 +0200

    Add files via upload

---
+++ display.php
@@ -0,0 +1,134 @@
+<?php
+
+// Information System
+function SystemGen()
+{
+    $Hostname = "Name Server : " . shell_exec("hostname");
+    $Uptime = "Time Running : " . shell_exec("uptime | cut -c 13-18");
+    $IpHost = "Ip System : " . shell_exec("ip a | grep ens18 | grep inet | cut -c 10-27");
+    /* $IpNetwork = "Ip Network : "  shell_exec("") <- En attente d'une commande shell ; */
+    $IpDiff = "Ip Diffusion : " . shell_exec("ip a | grep ens18 | grep inet | cut -c 33-48");
+    $Os = "Os : " . shell_exec("uname -a | cut -c 38-55");
+    $Memory = "Memory : " . shell_exec("free -g | grep Mem | cut -c 20");
+    $HardDiskSpaceFree = "Space Free Disk : " . shell_exec("df -h /dev/sda1 | grep /dev/sda1 | cut -c 18-20");
+    $HardDiskUse = "Use Space Disk : " . shell_exec("df -h /dev/sda1 | grep /dev/sda1 | cut -c 35-37");
+    $BackLine = "<br />";
+    $Result = $Hostname . $BackLine . $Uptime . $BackLine . $IpHost . $BackLine . $IpDiff . $BackLine . $Os . $BackLine . $Memory . $BackLine . $HardDiskSpaceFree . $BackLine . $HardDiskUse;
+    return $Result;
+}
+function Cpu()
+{
+    $CpuModel = "Name CPU  : " . shell_exec("lscpu | grep Model | grep name | cut -c 34-60");
+    $Architecture = "Architecture : " . shell_exec("lscpu | grep Architecture | cut -c 34-40");
+    $Bits = "Bits : " . shell_exec("lscpu | grep CPU | grep op-mode | cut -c 34-47");
+    $Ghz = "Ghz : " . shell_exec("lscpu | grep CPU | grep MHz | cut -c 34-45");
+    $Core = "Core : " . shell_exec("lscpu | grep Core | cut -c 34");
+    $Threads = "Threads : " . shell_exec("lscpu | grep Thread | cut -c 34");
+    $Socket = "Socket : " . shell_exec("lscpu | grep Socket | cut -c 34");
+    $BackLine = "<br />";
+    $Result = $CpuModel . $BackLine . $Architecture . $BackLine . $Bits . $BackLine . $Ghz . $BackLine . $Core . $BackLine . $Threads . $BackLine . $Socket;
+    return $Result;
+}
+
+// Seuil d'utilisation du CPU et mémoire
+function MemorySeuil()
+{
+    $UseMem = shell_exec("free -m | grep Mem | cut -c 28-35");
+    $Memory = round($UseMem);
+    
+    if ($Memory > 2000)
+    {
+        return "You are exceed the size memory";
+    }
+}
+function CpuSeuil()
+{
+    $Task = shell_exec("cat /proc/stat | grep processes | cut -c 11-1000");
+    #$Utime = shell_exec("cat /proc/uptime | cut -c 1-9");
+    #$Stime = shell_exec("cat /proc/uptime | cut -c 10-30");
+}
+
+// Ping vers les FAI et Hebergements Serveurs
+function PingFai()
+{
+    $Orange = shell_exec("ping -c 1 193.252.117.145 | grep 64 | cut -c 15-150");
+    $Free = shell_exec("ping -c 1 212.27.48.10 | grep 64 | cut -c 15-150");
+    $Sfr = shell_exec("ping -c 1 80.125.163.172 | grep 64 | cut -c 15-150");
+    $Bouygues = shell_exec("ping -c 1 194.158.122.212 | grep 64 | cut -c 15-150");
+
+    if ($_POST["Operateur"] == "Orange" && $_POST["PingOp"])
+    {
+        $Result = "<br />" . $Orange;
+        return $Result;
+    }
+    elseif ($_POST["Operateur"] == "Free" && $_POST["PingOp"])
+    {
+        $Result = "<br />" . $Free;
+        return $Result;
+    }
+    elseif ($_POST["Operateur"] == "Sfr" && $_POST["PingOp"])
+    {
+        $Result = "<br />" . $Sfr;
+        return $Result;
+    }
+    elseif ($_POST["Operateur"] == "Bouygues" && $_POST["PingOp"])
+    {
+        $Result = "<br />" . $Bouygues;
+        return $Result;
+    }
+}
+function PingHbg()
+{
+    $Ovh = shell_exec("ping -c 1 ovhcloud.com | grep 64 | cut -c 26-150");
+    $Hostinger = shell_exec("ping -c 1 hostinger.fr | grep 64 | cut -c 29-150");
+    $Informaniak = shell_exec("ping -c 1 infomaniak.com | grep 64 | cut -c 48-150");
+    $GoDaddy = shell_exec("ping -c 1 godaddy.com | grep 64 | cut -c 53-150");
+
+    if ($_POST["Hebergeur"] == "Ovh" && $_POST["PingHbg"])
+    {
+        $Result = "<br />" . $Ovh;
+        return $Result;
+    }
+    elseif ($_POST["Hebergeur"] == "Hostinger" && $_POST["PingHbg"])
+    {
+        $Result = "<br />" . $Hostinger;
+        return $Result;
+    }
+    elseif ($_POST["Hebergeur"] == "Informaniak" && $_POST["PingHbg"])
+    {
+        $Result = "<br />" . $Informaniak;
+        return $Result;
+    }
+    elseif ($_POST["Hebergeur"] == "GoDaddy" && $_POST["PingHbg"])
+    {
+        $Result = "<br />" . $GoDaddy;
+        return $Result;
+    }
+}
+
+function PingPersoIpEtDomaine()
+{
+    // Text Saisie
+    $Ip = $_POST["IpText"];
+    $Domaine = $_POST["DomaineText"];
+    
+    // Commande Linux
+    $PingIp = shell_exec("ping -c 1 $Ip | grep 64 | cut -c 15-150");
+    $PingDomaine = shell_exec("ping -c 1 $Domaine | grep 64");
+
+    // Condition
+    if ($_POST["PingPersoIp"])
+    {
+        $Result = "<br />" . $PingIp;
+        return $Result;
+    }
+    elseif ($_POST["PingPersoDomaine"])
+    {
+        $Result = "<br />" . $PingDomaine;
+    }
+
+}
+
+// Dépendance
+
+?>
---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+
+    <html lang="fr">
+      
+      <head>
+        <meta name="author" content="BARAS Gauthier" />
+        <meta charset="utf-8" />
+        <link href="index.css" rel="stylesheet" />
+        <title>Autosun</title>
+      </head>
+
+      <body>
+      
+      <!-- Fichier Display PHP -->
+      <?php include("display.php"); ?>
+
+        <div class="Head">
+           <ul class="NavBar">
+             <li>Dashboard</li>
+             <li>Information System</li>
+             <li>Task</li>
+             <li>Service</li>
+             <li>Security</li>
+             <li>Service</li>
+             <li>Data</li>
+             <li>Document</li>  
+           </ul> 
+        </div>
+
+        <div class="PingSystem">
+          <form class="Form" method="POST">
+            <h2 class="TitrePing">Vérification de communication</h2><br />
+              <h3 class="TitreOperateur">Opérateur</h3>
+                <select class="Operateur" name="Operateur">
+                  <option value="Orange">Orange</option>
+                  <option value="Free">Free</option>
+                  <option value="Sfr">Sfr</option>
+                  <option value="Bouygues">Bouygues</option>
+                </select><br />
+                <input type="submit" class="PingOp" name="PingOp" value="Verifier l'opérateur" /><br />
+                  <?php echo PingFai(); ?>
+              <h3 class="TitreHebergeurServ">Hebergeur</h3><br />
+                <select class="Hebergeur" name="Hebergeur">
+                  <option value="Ovh">Ovh</option>
+                  <option value="Hostinger">Hostniger</option>
+                  <option value="Informaniak">Informaniak</option>
+                  <option value="GoDaddy">GoDaddy</option>
+                </select><br />
+                <input type="submit" class="PingHbg" name="PingHbg" value="Verifier L'hebergeur" /><br />
+                  <?php echo PingHbg(); ?>
+              <h3 class="TitrePingPerso">Personnaliser</h3><br />
+                <input type="text" class="IpText" name="IpText" placeholder="ex : 192.168.1.1" /><br />
+                <input type="submit" class="PingPersoIp" name="PingPersoIp" value="Verifier L'IP" />
+                  <p class="ParagrapheOu">Ou</p>
+                <input type="text" class="DomaineText" name="DomaineText" placeholder="ex : example.com" /><br />
+                <input type="submit" class="PingPersoDomaine" name="PingPersoDomaine" value="Verifier Le Domaine" />
+                  <?php echo PingPersoIpEtDomaine(); ?>      
+          </form> 
+        </div>
+
+      </body>
+
+    </html>
