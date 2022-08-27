e14536f660f910ecae08b68f29fc0bdd3c15d839
Gauthier_5021 <autosun.barasg@gmail.com>
2022-08-27 17:50:49 +0200

    Test

--- nav.php
+++ nav.php
@@ -1,21 +1,31 @@
 <head>
  <meta name="author" content="BARAS Gauthier" />
  <meta charset="utf-8" />
- <link href="index.css" rel="stylesheet" />
  <title>Autosun</title>
+ <link href="index.css" rel="stylesheet" />
+ <!-- Boxicons CDN Link -->
+ 
 </head>
 
 <!-- Fichier Fonction PHP -->
 <?php include("fonction.php"); ?>
 
 <div class="Head">
-  <ul class="NavBar">
-    <li><a href="index.php" class="LienDashboard">Dashboard</a></li>
-    <li><a href="information.php" class="LienInformation">Information System</a></li>
-    <li><a href="log.php" class="LienLog">Log</a></li>
-    <li><a href="service.php" class="LienService">Service</a></li>
-    <li><a href="security.php" class="LienSecurity">Security</a></li>
-    <li>Data</li>
-    <li>Document</li>  
-  </ul> 
+    <ul class="NavBar">
+      <li><a href="index.php" class="LienDashboard">Dashboard</a></li>
+      <li><a href="information.php" class="LienInformation">Information System</a></li>
+      <li><a href="log.php" class="LienLog">Log</a></li>
+      <li><a href="service.php" class="LienService">Service</a></li>
+      <li><a href="security.php" class="LienSecurity">Security</a></li>
+      <li>Data</li>
+      <li>Document</li>  
+    </ul> 
+</div>
+
+<div class="sidebar">
+  <div class="logo_content">
+    <div class="logo">
+      <div class="logo_name">Gestionnaire Serveur</div>
+    </div>
+  </div>
 </div>
