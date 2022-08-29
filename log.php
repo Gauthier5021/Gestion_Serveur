318bde59a3b653095b03511523fa3392d19414cb
Gauthier_5021 <autosun.barasg@gmail.com>
2022-08-29 12:02:41 +0200

    Test

--- log.php
+++
@@ -1,8 +0,0 @@
-<!-- La NavBar PHP -->
-<?php include("Nav/nav.php"); ?>
-
-<!-- Information des utilisateurs -->
-<?php echo LogInfoUser(); ?>
-
-<!-- Information des logs -->
-<?php echo LogInfoSystem(); ?>
